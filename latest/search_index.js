bfa13cecc76da547ab61da8bb3ccbf374fd226ad
Documenter.jl <documenter@juliadocs.github.io>
2022-10-31 19:15:23 +0000

    build based on 50f032a

---
+++ latest/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SemioticOpt","category":"page"},{"location":"#SemioticOpt","page":"Home","title":"SemioticOpt","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SemioticOpt.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SemioticOpt]","category":"page"}]
+}
