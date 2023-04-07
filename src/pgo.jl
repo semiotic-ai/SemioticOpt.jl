ea58b66ac4e362352718542374279b628d2a8f15
Anirudh <anirudh@semiotic.ai>
2023-04-06 21:47:42 -0700

    feat: Compute the possible supports per iteration

--- src/pgo.jl
+++ src/pgo.jl
@@ -52,3 +52,93 @@ f(a::PGOOptFunction) = a.f
 The arguments to `f` that are not `x` or `ixs`.
 """
 args(a::PGOOptFunction) = a.args
+
+"""
+    possiblesupports(kmax::Integer, ixs::AbstractVector{<:Integer}, n::Integer)
+
+Compute the possible supports for this iteration of the outer loop of pgo.
+
+Here, `kmax` is the maximum support size, `ixs` is the current support, and `n` is length
+of the solution vector.
+
+# Example
+```julia
+julia> using SemioticOpt
+julia> ixs = 1:3 |> collect
+julia> n = 4
+julia> isfull = true
+julia> kmax = 3
+julia> out = SemioticOpt.possiblesupports(kmax, ixs, n)
+3×3 Matrix{Int32}:
+ 2  1  1
+ 3  3  2
+ 4  4  4
+```
+"""
+function possiblesupports(kmax::Integer, ixs::AbstractVector{<:Integer}, n::Integer)
+    k = length(ixs)
+    return possiblesupports(Val(k == kmax), k, ixs, n)
+end
+"""
+    possiblesupports(::Val{false}, k::Integer, ixs::AbstractVector{<:Integer}, n::Integer)
+
+Compute the possible supports for this iteration of the outer loop of pgo.
+
+Here, `k` is the current support size, `ixs` is the current support, and `n` is length
+of the solution vector.
+
+This method runs when the support is not full.
+
+# Example
+```julia
+julia> using SemioticOpt
+julia> ixs = 1:2 |> collect
+julia> n = 4
+julia> isfull = false
+julia> k = length(ixs)
+julia> out = SemioticOpt.possiblesupports(Val(isfull), k, ixs, n)
+3×2 Matrix{Int32}:
+ 1  1
+ 2  2
+ 3  4
+```
+"""
+function possiblesupports(::Val{false}, k::Integer, ixs::AbstractVector{<:Integer}, n::Integer)
+    nr = k + 1
+    nc = n - k
+    s = Matrix{Int32}(undef, nr, nc)'  # transpose to make it column major
+    s[:, 1:k] .= ixs'
+    s[:, k+1] .= setdiff(1:n, ixs)
+    return s'
+end
+"""
+    possiblesupports(::Val{true}, ixs::AbstractVector{<:Integer}, n::Integer)
+
+Return the possible supports for this iteration of the outer loop of pgo.
+
+Here, `k` is the current support size, `ixs` is the current support, and `n` is length
+of the solution vector.
+
+This method runs when the support is full.
+
+# Example
+```julia
+julia> using SemioticOpt
+julia> ixs = 1:2 |> collect
+julia> n = 4
+julia> isfull = true
+julia> k = length(ixs)
+julia> out = SemioticOpt.possiblesupports(Val(isfull), k, ixs, n)
+2×4 Matrix{Int32}:
+ 2  2  1  1
+ 3  4  3  4
+```
+"""
+function possiblesupports(::Val{true}, k::Integer, ixs::AbstractVector{<:Integer}, n::Integer)
+    nr = k
+    nc = k * (n - k)
+    s = Matrix{Int32}(undef, nr, nc)'  # transpose to make it column major
+    s[:, 1:(k-1)] .= repeat(repeatwithoutdiag(ixs)'; inner=(Int32(nc / k), 1))
+    s[:, k] .= repeat(setdiff(1:n, ixs), k)
+    return s'
+end
--- test/pgo.jl
+++ test/pgo.jl
@@ -15,4 +15,35 @@
         ixs = [1]
         @test _f(x, ixs, pgofunc |> SemioticOpt.args) == 1.0
     end
+
+    @testset "possiblesupports" begin
+        ixs = 1:2 |> collect
+        n = 4
+        isfull = false
+        k = length(ixs)
+        out = SemioticOpt.possiblesupports(Val(isfull), k, ixs, n)
+        @test out |> size == (k + 1, n - k)
+        @test out[:, 1] == [1, 2, 3]
+        @test out[:, 2] == [1, 2, 4]
+
+
+        isfull = true
+        out = SemioticOpt.possiblesupports(Val(isfull), k, ixs, n)
+        @test out |> size == (k, k * (n - k))
+        @test out[:, 1] == [2, 3]
+        @test out[:, 2] == [2, 4]
+        @test out[:, 3] == [1, 3]
+        @test out[:, 4] == [1, 4]
+
+        ixs = 1:3 |> collect
+        n = 4
+        isfull = true
+        kmax = 3
+        out = SemioticOpt.possiblesupports(kmax, ixs, n)
+        k = length(ixs)
+        @test out |> size == (k, k * (n - k))
+        @test out[:, 1] == [2, 3, 4]
+        @test out[:, 2] == [1, 3, 4]
+        @test out[:, 3] == [1, 2, 4]
+    end
 end
